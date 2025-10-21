b7cfa458267692d52d0c70baa380e4f8329e07cf
baisalikhan <112929397+baisalikhan@users.noreply.github.com>
2025-10-21 22:30:09 +0500

    feat: add the schemas and fix the convex issues

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,15 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
-import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import Header from "@/components/header";
+import { Toaster } from "sonner";
 
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Even Split",
-  description: "The simple and easy way to split expenses with friends.",
+  title: "Splitr",
+  description: "The smartest way to split expenses with friends",
 };
 
 export default function RootLayout({ children }) {
@@ -21,16 +18,18 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
-      <body
-        className={`${inter.className}`}
-      >
-        <ClerkProvider>
-        <ConvexClientProvider>
-          <Header />
+      <body className={`${inter.className}`}>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
+          <ConvexClientProvider>
+            <Header />
+            <main className="min-h-screen">
+              <Toaster richColors />
 
-          <main className="min-h-screen">{children}</main>
-
-        </ConvexClientProvider>
+              {children}
+            </main>
+          </ConvexClientProvider>
         </ClerkProvider>
       </body>
     </html>
